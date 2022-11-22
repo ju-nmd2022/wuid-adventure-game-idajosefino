52834b1c782b13310350c8060500082646f1f787
Ida <idajosefinolsson@gmail.com>
2022-11-22 22:29:45 +0100

    Emma är räddaren i nöden

--- frame1.js
+++ frame1.js
@@ -1,16 +1,41 @@
-const img = document.getElementById('book-image');
-const modal = document.getElementById('modal');
+const wandImageTrunk = document.getElementById('wand-image2');
+const bookImageTrunk = document.getElementById('book-image2');
+const bookImage = document.getElementById('book-image');
 const closeButton = document.getElementById('close-button');
+const modal = document.getElementById('modal');
+// const hufflepuffButton = document.getElementById('hufflepuff-button');
+//add link
 console.log(modal)
 
+checkLocalStorage()
 // modal.style.transform = 'translate(-50%, -50%) scale(1)';
+function checkLocalStorage(){
+    if(localStorage.getItem('wand')){
+        wandImageTrunk.style.visibility = 'visible'
+        // ravenclawButton.style.visibility = 'visible'
+    }
+    else{
+        wandImageTrunk.style.visibility = 'hidden'
+        // ravenclawButton.style.visibility = 'hidden'
+    }
+    if(localStorage.getItem('book')){
+        bookImageTrunk.style.visibility = 'visible'
+        bookImage.style.visibility = 'hidden'
+    }
+    else{
+        bookImageTrunk.style.visibility = 'hidden'
+    }
+}
 
-img.addEventListener('click', (event) => {
-    img.style.display = 'none';
+bookImage.addEventListener('click', (event) => {
+    bookImage.style.display = 'none';
     modal.style.transform = 'translate(-50%, -50%) scale(1)';
     console.log(modal)
+    localStorage.setItem('book', true);
+    checkLocalStorage()
   });
 
 closeButton.addEventListener('click', (event) => {
     modal.style.transform = 'translate(-50%, -50%) scale(0)';
 });
+//How to get the closed button to work so that I can use it on the other text boxes. 
--- frame5.js
+++ frame5.js
@@ -1,36 +1,85 @@
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const img = document.getElementById('wand-image');
-const trunk = document.getElementById('trunk');
+const wandImage = document.getElementById('wand-image');
+const wandImageTrunk = document.getElementById('wand-image2');
+const bookImageTrunk = document.getElementById('book-image2');
+const idaImage = document.getElementById('ida-image');
+const closeButton = document.getElementById('close-button');
+const modal = document.getElementById('modal');
+const ravenclawButton = document.getElementById('ravenclaw-button');
+console.log(modal)
 
-openModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal = document.querySelector(button.dataset.modalTarget)
-        openModal(modal)
-    })
-})
-
-closeModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal = button.closest('.modal')
-        closeModal(modal)
-    })
-})
-
-function openModal(modal) {
-    if (modal == null) return
-    modal.classList.add('active')
+checkLocalStorage()
+// modal.style.transform = 'translate(-50%, -50%) scale(1)';
+function checkLocalStorage(){
+    if(localStorage.getItem('wand')){
+        wandImageTrunk.style.visibility = 'visible'
+        wandImage.style.visibility = 'hidden'
+        ravenclawButton.style.visibility = 'visible'
+    }
+    else{
+        wandImageTrunk.style.visibility = 'hidden'
+        ravenclawButton.style.visibility = 'hidden'
+    }
+    if(localStorage.getItem('book')){
+        bookImageTrunk.style.visibility = 'visible'
+    }
+    else{
+        bookImageTrunk.style.visibility = 'hidden'
+    }
 }
+//move over wand to frame 1
 
-function closeModal(modal) {
-    if (modal == null) return
-    modal.classList.remove('active')
-};
+wandImage.addEventListener('click', (event) => {
+    wandImage.style.display = 'none';
+    localStorage.setItem('wand', true);
+    checkLocalStorage()
+  });
+
+closeButton.addEventListener('click', (event) => {
+    modal.style.transform = 'translate(-50%, -50%) scale(0)';
+});
+
+idaImage.addEventListener('click', (event) => {
+    modal.style.transform = 'translate(-50%, -50%) scale(1)';
+    console.log('Hej')
+});
+
+
+
+
+
+// const openModalButtons = document.querySelectorAll('[data-modal-target]')
+// const closeModalButtons = document.querySelectorAll('[data-close-button]')
+// const img = document.getElementById('wand-image');
+// const trunk = document.getElementById('trunk');
+
+// openModalButtons.forEach(button => {
+//     button.addEventListener('click', () => {
+//         const modal = document.querySelector(button.dataset.modalTarget)
+//         openModal(modal)
+//     })
+// })
+
+// closeModalButtons.forEach(button => {
+//     button.addEventListener('click', () => {
+//         const modal = button.closest('.modal')
+//         closeModal(modal)
+//     })
+// })
+
+// function openModal(modal) {
+//     if (modal == null) return
+//     modal.classList.add('active')
+// }
+
+// function closeModal(modal) {
+//     if (modal == null) return
+//     modal.classList.remove('active')
+// };
 //https://www.youtube.com/watch?v=MBaw_6cPmAw
 
-img.addEventListener('click', (event) => {
-  img.style.display = 'none';
-});
+// img.addEventListener('click', (event) => {
+//   img.style.display = 'none';
+// });
 
 //https://bobbyhadz.com/blog/javascript-hide-button-after-click
 
@@ -42,5 +91,4 @@ img.addEventListener('click', (event) => {
 //       txt = "You pressed Cancel!";
 //     }
 //     document.getElementById("wand-image").innerHTML = txt;
-//     localStorage.setItem('trunk', 'wand');
 //   }
